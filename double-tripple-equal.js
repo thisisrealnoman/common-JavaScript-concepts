016da7ebf2d1deb4d49f6f2b4725b29f5b29c1aa
thisisrealnoman <nomanontheroad@gmail.com>
2024-11-10 02:23:57 +0600

    double equal, triple equal

---
+++ double-tripple-equal.js
@@ -0,0 +1,18 @@
+// always use ===
+// equal comparison doesnt work for non primitive
+
+
+const first = true;
+const second = 1;
+// const first = [];
+// const second = [];
+if(first === second){
+    console.log('values are equal');
+}
+else{
+    console.log('values are not equal');
+}
+
+// type coercion
+// type conversion
+// type casting
